d9829adc7ed28bf41307d595ce3a62f9301983c4
AlanovAibek <alanov.aibek@gamail.com>
2017-11-06 22:26:03 +0300

    run_vae.py: 	PEP8 changes, add auxilary options

--- python/run_vae.py
+++ python/run_vae.py
@@ -1,9 +1,20 @@
+#!/usr/bin/env python3
 import numpy as np
 import tensorflow as tf
+from datetime import datetime
+from optparse import OptionParser
+import sys
+import os
+import warnings
+
 import matplotlib
 matplotlib.use('Agg')
-import matplotlib.pyplot as plt
-import seaborn as sns
+import matplotlib.pyplot as plt  # noqa
+import seaborn as sns  # noqa
+
+sys.path.append('../python/')
+from utils import train_model, test_model  # noqa
+from utils import get_fixed_mnist, setup_vaes_and_params, makedirs  # noqa
 
 np.random.seed(1234)
 tf.set_random_seed(1234)
@@ -13,26 +24,13 @@ sns.set_context(
     font_scale=2.0,
     rc={'lines.linewidth': 2, 'lines.markersize': 10, 'figsize': (5, 4.8)})
 
-from tensorflow.examples.tutorials.mnist import input_data 
-from collections import defaultdict
-from tqdm import tqdm
-from datetime import datetime
-from optparse import OptionParser
-import sys
-sys.path.append('../python/')
-import os
-
 tf.logging.set_verbosity(tf.logging.ERROR)
 tf.logging.set_verbosity(tf.logging.WARN)
 tf.logging.set_verbosity(tf.logging.DEBUG)
 tf.logging.set_verbosity(tf.logging.FATAL)
 
-import warnings
 warnings.filterwarnings('ignore', module='matplotlib')
 
-from vae import VAE, LogDerTrickVAE, VIMCOVAE, NVILVAE, MuPropVAE, GumbelSoftmaxTrickVAE
-from utils import train_model, test_model, consider_stds, get_gradient_mean_and_std
-from utils import binarized_mnist_fixed_binarization, setup_input_vaes_and_params, makedirs
 
 def main():
     parser = OptionParser(usage="usage: %prog [options]",
@@ -41,23 +39,33 @@ def main():
     parser.add_option("--n_ary", type='int', help="arity of latent variable")
     parser.add_option("--en_dist", type='string', help="encoder distribution")
     parser.add_option("--l_r", type='float', help="learning rate")
-    parser.add_option("--n_samples", type='int', help="train objective samples")
+    parser.add_option("--n_samples", type='int',
+                      help="train objective samples")
+    parser.add_option("test_b_size", type='int', default=1024,
+                      help="test batch size")
+    parser.add_option("--test_n_samples", type='int', default=5,
+                      help="test objective samples")
     parser.add_option("--c_devs", type='string', help="cuda devices")
-    parser.add_option("--mem_frac", type='float', default=0.5, help="memory fraction used in gpu")
+    parser.add_option("--mem_frac", type='float', default=0.5,
+                      help="memory fraction used in gpu")
+    parser.add_option("--mode", type='string', default='train',
+                      help='train or test')
     (options, args) = parser.parse_args()
-    logging_file = '-'.join(sorted(['{}{}'.format(k, v) for k, v in vars(options).items()]))
+    options_to_str = ['{}{}'.format(k, v) for k, v in vars(options).items()]
+    logging_file = '-'.join(sorted(options_to_str))
     logging_file += datetime.now().strftime("_%H:%M:%S") + '.txt'
     log_dir = os.path.join('logs', datetime.now().strftime("%Y-%m-%d"))
     makedirs(log_dir)
     logging_path = os.path.join(log_dir, logging_file)
-    
-    binarized_mnist = binarized_mnist_fixed_binarization('datasets/', validation_size=10000)
-    X_train, X_val, X_test = binarized_mnist.train, binarized_mnist.validation, binarized_mnist.test
+
+    mnist = get_fixed_mnist('datasets/',
+                            validation_size=10000)
+    X_train, X_val, X_test = mnist.train, mnist.validation, mnist.test
 
     params = {
         'X_train': X_train,
         'X_test': X_test,
-        'X_val': X_val, 
+        'X_val': X_val,
         'dataset': 'BinaryMNIST',
         'n_z': options.n_z,
         'n_ary': options.n_ary,
@@ -66,21 +74,22 @@ def main():
         'train_batch_size': 128,
         'train_obj_samples': options.n_samples,
         'val_batch_size': 1024,
-        'val_obj_samples': 5, 
-        'test_batch_size': 1024,
-        'test_obj_samples': 5,        
+        'val_obj_samples': 5,
+        'test_batch_size': options.test_b_size,
+        'test_obj_samples': options.test_n_samples,
         'cuda_devices': options.c_devs,
         'save_step': 100,
         'n_epochs': 3001,
         'save_weights': True,
         'mem_fraction': options.mem_frac,
-        'all_vaes': True, 
-        'mode': 'train', 
+        'all_vaes': True,
+        'mode': options.mode,
         'results_dir': 'test_results',
         'logging_path': logging_path
     }
 
-    train_model(**setup_input_vaes_and_params(**params))
+    train_model(**setup_vaes_and_params(**params))
+
 
 if __name__ == '__main__':
     main()
