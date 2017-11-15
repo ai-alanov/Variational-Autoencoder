2d038ad2a2bded18b933576350c578206145b930
AlanovAibek <alanov.aibek@gmail.com>
2017-11-15 13:50:56 +0300

    train_process.py: 	hotfix

--- python/train_process.py
+++ python/train_process.py
@@ -145,7 +145,7 @@ def load_loss(vaes, save_dir, results_dir, loss_name):
 
 
 def save_loss(vaes, loss, save_dir, results_dir, loss_name, epochs=None):
-    file_name = loss['test_samples'] + '-' + vaes[0].dataset_name()
+    file_name = str(loss['test_samples']) + '-' + vaes[0].dataset_name()
     file_name += '-' + vaes[0].parameters()
     other_save_path = os.path.join(
         results_dir, file_name)
