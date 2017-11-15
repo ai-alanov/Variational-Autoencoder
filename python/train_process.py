183c422aa77fedb5267d49b6360946dca8c54d9e
AlanovAibek <alanov.aibek@gmail.com>
2017-11-15 13:56:44 +0300

    train_process.py: 	hotfix

--- python/train_process.py
+++ python/train_process.py
@@ -145,12 +145,13 @@ def load_loss(vaes, save_dir, results_dir, loss_name):
 
 
 def save_loss(vaes, loss, save_dir, results_dir, loss_name, epochs=None):
-    file_name = str(loss['test_samples']) + '-' + vaes[0].dataset_name()
-    file_name += '-' + vaes[0].parameters()
-    other_save_path = os.path.join(
-        results_dir, file_name)
-    with open(other_save_path, 'wb') as f:
-        pickle.dump(loss, f)
+    if 'test_samples' in loss:
+        file_name = str(loss['test_samples']) + '-' + vaes[0].dataset_name()
+        file_name += '-' + vaes[0].parameters()
+        other_save_path = os.path.join(
+            results_dir, file_name)
+        with open(other_save_path, 'wb') as f:
+            pickle.dump(loss, f)
     for vae in vaes:
         save_path = os.path.join(save_dir, vae.name(), vae.dataset_name(),
                                  vae.parameters())
