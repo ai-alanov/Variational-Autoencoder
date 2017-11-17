2a12b60d82fd7e790f6f41c81875a34cba75afd6
AlanovAibek <alanov.aibek@gmail.com>
2017-11-17 16:34:00 +0300

    train_process.py: 	hotfix (cast lr to float in save_loss)

--- python/train_process.py
+++ python/train_process.py
@@ -164,7 +164,7 @@ def save_loss(vaes, loss, save_dir, results_dir, loss_name,
             pickle.dump(loss, f)
     for lr in loss.keys():
         for vae in vaes:
-            save_path, now = find_file(vae, save_dir, lr)
+            save_path, now = find_file(vae, save_dir, float(lr))
             save_path = os.path.join(save_path, now, results_dir)
             makedirs(save_path)
             file_name = loss_name
