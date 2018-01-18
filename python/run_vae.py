002b789d70297da614723d4ccebbb058ee7d1730
AlanovAibek <alanov.aibek@gmail.com>
2018-01-18 17:41:01 +0300

    run_vae.py: 	add n_stages, stage_to_epochs, lr_decay arguments

--- python/run_vae.py
+++ python/run_vae.py
@@ -51,6 +51,34 @@ def nonlinearity_callback(option, opt, value, parser):
         raise ValueError('Invalid nonlinearity: {}'.format(value))
 
 
+def stage_to_epochs_callback(option, opt, value, parser):
+    func, parameter = value.split(',')
+    parameter = int(parameter)
+    if func == 'lin':
+        setattr(parser.values, option.dest, lambda x: parameter * x)
+    elif func == 'exp':
+        setattr(parser.values, option.dest, lambda x: parameter ** x)
+    else:
+        error_message = 'Invalid function type (only \'lin\', \'exp\'): {}'
+        raise ValueError(error_message.format(func))
+
+
+def lr_decay_callback(option, opt, value, parser):
+    func, *parameters = value.split(',')
+    parameters = list(map(lambda x: int(x), parameters))
+    if func == 'lin':
+        setattr(parser.values, option.dest, lambda x: x / parameters[0])
+    elif func == 'exp':
+        setattr(parser.values, option.dest, lambda x: parameters[0] ** (-x))
+    elif func == 'linexp':
+        setattr(parser.values, option.dest,
+                lambda x: parameters[0] ** (-x / parameters[1]))
+    else:
+        error_message = 'Invalid function type '
+        error_message += '(only \'lin\', \'exp\', \'linexp\'): {}'
+        raise ValueError(error_message.format(func))
+
+
 def main():
     parser = OptionParser(usage="usage: %prog [options]",
                           version="%prog 1.0")
@@ -83,6 +111,8 @@ def main():
                       help="number of iterations for gradient std estimation")
     parser.add_option("--save_step", type='int', default=100,
                       help="step of saving weights")
+    parser.add_option("--display_step", type='int', default=100,
+                      help="step of logging results")
     parser.add_option("--nonlinearity", type='string', default=tf.nn.softplus,
                       help="nonlinearity for VAE", action='callback',
                       callback=nonlinearity_callback)
@@ -91,11 +121,22 @@ def main():
                       callback=split_by_comma_callback)
     parser.add_option("--n_epochs", type='int', default=3001,
                       help="number of training epochs")
+    parser.add_option("--n_stages", type='int', default=None,
+                      help="number of training stages")
+    parser.add_option("--stage_to_epochs", type='string', default=lambda x: 1,
+                      help='''function which mapes a stage to the number of
+                      epochs, options: lin,a -> a*stage; exp,a -> a**stage''',
+                      action='callback', callback=stage_to_epochs_callback)
     parser.add_option("--valid_size", type='int', default=1345,
                       help="validation size")
+    parser.add_option("--lr_decay", type='string', default=lambda x: 1,
+                      help='''function which mapes a stage to the learning rate
+                      decay, options: lin,a -> stage/a; exp,a -> a**(-stage),
+                      linexp,a,b -> a**(-stage/b)''',
+                      action='callback', callback=lr_decay_callback)
     (options, args) = parser.parse_args()
     options_to_str = ['{}:{}'.format(k[:3], v)
-                      for k, v in vars(options).items()]
+                      for k, v in vars(options).items() if not callable(v)]
     logging_file = '-'.join(sorted(options_to_str))
     logging_file += datetime.now().strftime("_%H:%M:%S") + '.txt'
     log_dir = os.path.join('logs', datetime.now().strftime("%Y-%m-%d"))
@@ -127,7 +168,9 @@ def main():
         'test_obj_samples': options.test_n_samples,
         'cuda_devices': options.c_devs,
         'save_step': options.save_step,
-        'n_epochs': options.n_epochs,
+        'display_step': options.display_step,
+        'n_stages': options.n_stages if options.n_stages else options.n_epochs,
+        'stage_to_epochs': options.stage_to_epochs,
         'save_weights': True,
         'mem_fraction': options.mem_frac,
         'mode': options.mode,
@@ -136,7 +179,8 @@ def main():
         'learning_rates': options.l_rs,
         'vaes_to_choose': options.vaes,
         'n_iterations': options.n_iters,
-        'weights': options.weights
+        'weights': options.weights,
+        'lr_decay': options.lr_decay
     }
     if options.mode == 'train':
         train_model(**setup_vaes_and_params(**params))
